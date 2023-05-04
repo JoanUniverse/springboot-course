8e2815b216ef91372d2068bcc58293d4c8b22a56
Joan Quetglas <soporte.joan@galileus.es>
2023-05-04 13:59:41 +0200

    First commit

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/test/java/com/udemy/springboot/demo/springudemy/SpringudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springboot.demo.springudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
