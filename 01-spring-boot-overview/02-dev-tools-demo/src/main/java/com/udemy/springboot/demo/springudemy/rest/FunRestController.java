fe6d57ae6d7cd0a8a2d729eb73ba90c1bdb5ce50
Joan Quetglas <soporte.joan@galileus.es>
2023-05-04 15:58:43 +0200

    SpringBoot starter dependencies: DevTools, Actuator, Security

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/udemy/springboot/demo/springudemy/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/udemy/springboot/demo/springudemy/rest/FunRestController.java
@@ -1,13 +1,27 @@
 package com.udemy.springboot.demo.springudemy.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${jo.mateix}")
+    private String name;
+
     @GetMapping("/")
     public String sayHello() {
-        return  "Hello";
+        return  "Hello " + name;
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "workout";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "lucky!";
     }
 }
