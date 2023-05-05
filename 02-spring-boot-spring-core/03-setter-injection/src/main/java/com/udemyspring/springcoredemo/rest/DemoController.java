544f0cc62703f4eddbc2fd3c48dd1114d56f7703
Joan Quetglas <soporte.joan@galileus.es>
2023-05-05 09:22:41 +0200

    Setter injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/udemyspring/springcoredemo/rest/DemoController.java
@@ -0,0 +1,27 @@
+package com.udemyspring.springcoredemo.rest;
+
+import com.udemyspring.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+//    @Autowired
+//    public DemoController(Coach theCoach) {
+//        myCoach = theCoach;
+//    }
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
