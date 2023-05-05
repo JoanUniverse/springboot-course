48f9643880ed099eb08ab87888958a13f8800e3f
Joan Quetglas <soporte.joan@galileus.es>
2023-05-05 09:41:22 +0200

    @Qualifier annotation

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/udemyspring/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.udemyspring.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backend volley";
+    }
+}
---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/udemyspring/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,12 @@
+package com.udemyspring.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run for 5km";
+    }
+}
