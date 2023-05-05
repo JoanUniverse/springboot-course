c8e7e176e02c61bb8b76368c63da6343d24f675d
Joan Quetglas <soporte.joan@galileus.es>
2023-05-05 15:57:47 +0200

    Student DAO

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,44 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello");
+//			createStudent(studentDAO);
+
+//			createMultipleStudents(studentDAO);
+			readStudent(studentDAO);
 		};
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+		Student tempStudent = new Student("Daffy", "Duck", "duck@gmail.com");
+
+		studentDAO.save(tempStudent);
+
+		int id = tempStudent.getId();
+		System.out.println("ID " + id);
+
+		Student myStudent = studentDAO.findById(id);
+		System.out.println(myStudent);
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		Student tempStudent1 = new Student("Jo", "Mateix", "joan@gmail.com");
+		Student tempStudent2 = new Student("Marc", "Public", "marc@gmail.com");
+		Student tempStudent3 = new Student("Bonita", "Isla", "bonita@gmail.com");
+
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating student...");
+		Student tempStudent = new Student("Jo", "Mateix", "joan@gmail.com");
+
+		System.out.println("Saving student...");
+		studentDAO.save(tempStudent);
+
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
